bcb39979741c9d9970c5889face2729d6b1d0398
Doreen <doreenlulu97@gmail.com>
2022-08-15 23:31:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet() {
   // Write your code here!
+  let result
+  if (199 <= 400) {
+    result = 'This one is on me!';
+  }
+  else if (2001>2000) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else {
+    result = 'No can do.';
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity() {
   // Write your code here!
+  let city
+  if (city = 'NYC') {
+    city = 'Ok, sounds good.';
+  }
+  else {
+  city = 'No go.';
+  }
+  return city
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
+  let tip
+  if (tip = 'generous') {
+    tip = 'Thank you so much.';
+  }
 }
